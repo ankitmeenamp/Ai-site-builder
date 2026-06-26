7bcad91db0e50a184bef777d81a210a2d546f33e
Ankit meena <ankitmeenapc1@gmail.com>
2026-06-26 10:57:48 +0530

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,46 @@
+
+import { Route, Routes, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Projects from "./pages/Projects";
+import MyProject from "./pages/MyProject";
+import Preview from "./pages/Preview";
+
+import View from "./pages/View";
+import Navbar from "./components/Navbar";
+import Community from "./pages/Cummunity";
+import { Toaster } from "sonner";
+import Authpage from "./pages/auth/AuthPage";
+import Settings from "./pages/Settings";
+
+
+function App() {
+
+  const {pathname} = useLocation()
+
+  const hideNavbar = pathname.startsWith('/projects/') && pathname !== '/projects' 
+  || pathname.startsWith('/view/')
+  || pathname.startsWith('/preview/')
+
+  return (
+    <div>
+    <Toaster />
+      {!hideNavbar && <Navbar />}
+  
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProject />} />
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+        <Route path="/auth/:pathname" element={<Authpage />} />
+        <Route path="/account/settings" element={<Settings/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.ts
@@ -0,0 +1,30 @@
+import  express, { Request, Response }  from "express";
+import 'dotenv/config'
+import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
+
+const app = express();
+
+const port = 3000;
+
+const corsOptions={
+    origin: process.env.TRUSTED_ORIGINS?.split(',') || [],
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Server is Live!');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`);
+    
+})
+
+console.log("TRUSTED_ORIGINS =", process.env.TRUSTED_ORIGINS);
+console.log("CORS ORIGINS =", corsOptions.origin);
