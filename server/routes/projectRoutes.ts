56b907b1886f9ab6a3202dec4963c7109e0758c2
Ankit meena <ankitmeenapc1@gmail.com>
2026-06-29 14:20:54 +0530

    backend

--- server/routes/projectRoutes.ts
+++ server/routes/projectRoutes.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import { protect } from "../middlewares/auth.js";
-import { makeRevision } from "../controllers/projectController.js";
+import { deleteProject, getProjectById, getProjectPreview, getPublishedProjects, makeRevision, rollbackToVersion, saveProjectCode } from "../controllers/projectController.js";
 
 const projectRouter = express.Router();
 
 projectRouter.post('/revision/:projctId',protect, makeRevision) 
+projectRouter.put('/save/:projctId',protect, saveProjectCode) 
+projectRouter.get('/rollback/:projctId/:versionId',protect, rollbackToVersion) 
+projectRouter.delete('/:projctId',protect, deleteProject) 
+projectRouter.get('/preview/:projctId',protect, getProjectPreview) 
+projectRouter.get('/published', getPublishedProjects) 
+projectRouter.get('/published/:projectId', getProjectById) 
+
+export default projectRouter ;
